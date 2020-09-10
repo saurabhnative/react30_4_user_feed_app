8a59ca8e8c6458d678e6d8dc2cb40d2a5ea5b3b1
Saurabh Native <codeclassifiers@gmail.com>
2020-09-10 23:28:26 +0530

    test cases added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, fireEvent, getByTestId} from "@testing-library/react";
+import App from "./App.js";
+import Header from "./components/Header";
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it("App loads with initial title as empty string", () => {
+  const { container } = render(<Header />);
+  const title = getByTestId(container, "title");
+  expect(title.textContent).toBe("");
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
       <div className="container">
         <div className="row m-auto">
           <i className="fa fa-hand-pointer-o fa-2x text-white mr-2" />
-          <div className="text-light h2">{props.title}</div>
+          <div className="text-light h2" data-testid="title">{props.title}</div>
         </div>
       </div>
     </nav>
