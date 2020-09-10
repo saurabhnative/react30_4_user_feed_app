ccbd1a8281de0dd16b7d24acbe9666754ff1c615
Saurabh Native <codeclassifiers@gmail.com>
2020-09-10 22:28:26 +0530

    user feed question added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import UserList from "./components/UserList";
+import Header from "./components/Header";
+export default function App() {
+  const [data, updateData] = useState([])
+  useEffect(()=>{
+    // API call to get user list
+  },[]) 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <Header title={"UserList"} />
+        <div className="row d-flex justify-content-center">
+          {data.map((item, index) => (
+            <UserList key={index} item={item} />
+          ))}
+        </div>
+      </div>
+    );
 }
-
-export default App;
---
+++ src/components/UserList.js
@@ -0,0 +1,10 @@
+import React from "react";
+function UserList({ item,index }) {
+    return (
+        <div className="col-sm-12 d-flex justify-content-center">
+            <div className="card" style={{ width: "18rem" }}>
+            </div>
+        </div>
+    )
+}
+export default UserList;
